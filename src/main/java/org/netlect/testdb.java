d691d4071657939631fe6178e993e59999cc4d75
netlect <Rafopenshift@gmail.com>
2017-07-09 19:31:56 +0600

    Update testdb.java

--- src/main/java/org/netlect/testdb.java
+++ src/main/java/org/netlect/testdb.java
@@ -16,14 +16,20 @@ public String getTestName() {
     try {
 
         String databaseURL = "jdbc:postgresql://";
-databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
-databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
-String username = System.getenv("POSTGRESQL_USER");
-String password = System.getenv("PGPASSWORD");
-Connection connection = DriverManager.getConnection(databaseURL, username, password);
-if (connection != null) {
-String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
-Statement stmt = connection.createStatement();
+        databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
+        databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
+        String username = System.getenv("POSTGRESQL_USER");
+        String password = System.getenv("PGPASSWORD");
+
+        Connection connection = DriverManager.getConnection(databaseURL, username, password);
+
+        if (connection != null) {
+
+     //       String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
+
+            String SQL = "select name from test where id=1";        
+            
+            Statement stmt = connection.createStatement();
 ResultSet rs = stmt.executeQuery(SQL);
 while (rs.next()) {
 if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
